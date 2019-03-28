4b2e3264c1a191cc185415887737328134312cec
JackWong992 <JackWong992@163.com>
2019-03-28 21:27:25 +0800

    finish twenty-three name

---
+++ test/23_命名规则.py
@@ -0,0 +1,25 @@
+"""
+1. 只能由数字字母下划线组成
+2. 不能以数字开头
+3. 不能是关键字
+4. 区分大小写
+
+a true
+666 false
+ak47 true
+hello true
+Hi true
+name true
+first_name true
+last_name true
+_name true
+good@itcast.cn false
+#itcast false
+good!good  false
+(_0_) false
+______ true
+-_-! false
+关键字:
+and class elif for import nonlocal raise with False as
+continue else from in not return yield None assert
+"""
