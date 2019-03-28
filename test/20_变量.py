ba5afa183c5be44be46b15908baaba66f05ca48f
JackWong992 <JackWong992@163.com>
2019-03-28 17:44:52 +0800

    learning python

---
+++ test/20_变量.py
@@ -0,0 +1,12 @@
+"""
+变量： 用于保存计算机的数据
+定义格式: 变量名 = 值
+"""
+
+age = 18
+height = 1.80
+weight = 80.0
+
+age = 19
+height = 1.9
+print(age, height, weight)
---
+++ test/demo1.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("hello world for one")
+print("hello world for two")
+
